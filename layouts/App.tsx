1bb69102e4aa9a38be3b9a4729d24a82ffd125dc
jimin <jm131313.kim@gmail.com>
2022-03-02 22:36:04 +0900

    first commit

---
+++ layouts/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import loadable from '@loadable/component';
+
+const Login = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
