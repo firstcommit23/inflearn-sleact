660513df0d87818a33f7cc4147362ebaee3409bb
jimin <jm131313.kim@gmail.com>
2022-03-09 17:31:14 +0900

    add: db interface 추가

---
+++ typings/db.ts
@@ -0,0 +1,44 @@
+export interface IUser {
+  id: number;
+  nickname: string;
+  email: string;
+  Workspaces: IWorkspace[];
+}
+
+export interface IUserWithOnline extends IUser {
+  online: boolean;
+}
+
+export interface IChannel {
+  id: number;
+  name: string;
+  private: boolean;
+  WorkspaceId: number;
+}
+
+export interface IChat {
+  id: number;
+  UserId: number;
+  User: IUser;
+  content: String;
+  createAt: Date;
+  ChannelId: number;
+  Channel: IChannel;
+}
+
+export interface IDM {
+  id: number;
+  SenderId: number;
+  Sender: IUser;
+  ReveiverId: number;
+  Revevier: IUser;
+  content: string;
+  createdAt: Date;
+}
+
+export interface IWorkspace {
+  id: number;
+  name: string;
+  url: string;
+  OwnerId: number;
+}
