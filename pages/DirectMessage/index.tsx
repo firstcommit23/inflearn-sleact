45f5065684fbd8cabeaab6bb4aad845ac15bef08
jimin <jm131313.kim@gmail.com>
2022-03-20 17:05:45 +0900

    feat: DM 채팅보내기

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -1,22 +1,42 @@
 import React, { useCallback } from 'react';
 import { useParams } from 'react-router';
 import useSWR from 'swr';
+import { IDM } from '@typings/db';
 import gravatar from 'gravatar';
 import useInput from '@hooks/useInput';
 import fetcher from '@utils/fetcher';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
 import { Container, Header } from '@pages/DirectMessage/styles';
+import axios from 'axios';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR<any>(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR<any>('/api/users', fetcher);
+  const {
+    data: chatData,
+    mutate: mutateChat,
+    revalidate,
+  } = useSWR<IDM[]>(`/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`, fetcher);
 
-  const [chat, onChangeChat] = useInput('');
-  const onSubmitForm = useCallback((e: any) => {
-    e.preventDefault();
-  }, []);
+  const [chat, onChangeChat, setChat] = useInput('');
+  const onSubmitForm = useCallback(
+    (e: any) => {
+      console.log(chat);
+      e.preventDefault();
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, { content: chat })
+          .then(() => {
+            revalidate();
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat, revalidate],
+  );
   if (!userData || !myData) return null;
 
   return (
