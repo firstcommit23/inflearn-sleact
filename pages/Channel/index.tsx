f4817883cb6e85dc8a698025e85d1ee0d42d9114
jimin <jm131313.kim@gmail.com>
2022-03-20 15:27:15 +0900

    feat: 채팅박스 만들기

--- pages/Channel/index.tsx
+++ pages/Channel/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import useInput from '@hooks/useInput';
+import ChatList from '@components/ChatList';
+import ChatBox from '@components/ChatBox';
 import { Container, Header } from '@pages/Channel/styles';
 
 const Channel = () => {
+  const [chat, onChangeChat, setChat] = useInput('');
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    setChat('');
+  }, []);
   return (
     <Container>
       <Header>채널</Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} placeholder="입력해 주세요.." />
     </Container>
   );
 };
--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useParams } from 'react-router';
+import useSWR from 'swr';
+import gravatar from 'gravatar';
+import useInput from '@hooks/useInput';
+import fetcher from '@utils/fetcher';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import { Container, Header } from '@pages/DirectMessage/styles';
 
 const DirectMessage = () => {
-  return <div>DM 페이지~</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR<any>(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR<any>('/api/users', fetcher);
+
+  const [chat, onChangeChat] = useInput('');
+  const onSubmitForm = useCallback((e: any) => {
+    e.preventDefault();
+  }, []);
+  if (!userData || !myData) return null;
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList></ChatList>
+      <ChatBox
+        chat={chat}
+        onSubmitForm={onSubmitForm}
+        onChangeChat={onChangeChat}
+        placeholder="입력해 주세요"
+      ></ChatBox>
+    </Container>
+  );
 };
 
 export default DirectMessage;
